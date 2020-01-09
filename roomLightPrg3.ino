c36d9dc5c4a5bf10d6e8d22d4b8736af17ec4ddf
Nitish Chennoju <nchennoju@gmail.com>
2020-01-09 11:30:21 -0800

    Add files via upload

---
+++ roomLightPrg3.ino
@@ -0,0 +1,53 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define LED BUILTIN_LED
+
+char auth[] = "jwEgv4MqX-Y5nUEbMFFM8PJT5_dZoq6n";
+
+char ssid[] = "homenetwork";
+char pass[] = "4085075811";
+
+String lastMsg = "";
+int state = 0;
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+BLYNK_CONNECTED() {
+    Blynk.syncAll();
+}
+
+BLYNK_WRITE(V0) {
+  lastMsg = param.asStr();
+  Serial.println("Message: " + lastMsg);
+  Blynk.virtualWrite(V0, "clr");
+  Blynk.virtualWrite(V0, "Last Message:\n\n" + lastMsg);
+}
+
+BLYNK_WRITE(V1) {
+    state = param.asInt();
+    Serial.println("ON/OFF State: " + String(state));
+    if(state == 0){
+      digitalWrite(LED, LOW);
+    }else{
+      digitalWrite(LED, HIGH);
+    } 
+}
+
+void loop()
+{
+  Blynk.run();
+}
